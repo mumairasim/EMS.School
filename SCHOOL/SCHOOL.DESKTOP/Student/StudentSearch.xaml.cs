3539f73986624a53736fa3f39cc13071c451b8a7
M Umair Asim <mumairasim@hotmail.com>
2020-04-18 18:34:49 +0500

    Initial Setup

---
+++ SCHOOL/SCHOOL.DESKTOP/Student/StudentSearch.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SCHOOL.DESKTOP.Student
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StudentSearch : Window
+    {
+        public StudentSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
