b3685d29d701b26993aa7c21b7baca68f8484beb
M Umair Asim <mumairasim@hotmail.com>
2020-09-24 19:45:27 +0500

    Minor changes

--- SCHOOL/SCHOOL.DESKTOP/MainWindow.xaml.cs
+++ SCHOOL/SCHOOL.DESKTOP/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using MaterialDesignThemes.Wpf;
+using SCHOOL.DTOs.DTOs;
 using SCHOOL.Services.Implementation;
 using SCHOOL.Services.Infrastructure;
 
@@ -10,10 +14,30 @@ namespace SCHOOL.DESKTOP
     public partial class MainWindow : Window
     {
         private readonly IClassService _classService;
+        public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
+        public List<Class> ClassList { get; set; } = new List<Class>();
         public MainWindow(IClassService classService)
         {
             _classService = classService;
             InitializeComponent();
+            FetchClasses();
+            Items.Add(new Item
+            {
+                Name = "Printer",
+                IconKind = PackIconKind.Printer
+            });
+
+            Items.Add(new Item
+            {
+                Name = "AbTesting",
+                IconKind = PackIconKind.AbTesting
+            });
+
+            Items.Add(new Item
+            {
+                Name = "GoogleHome",
+                IconKind = PackIconKind.GoogleHome
+            });
         }
 
         private void StudentTab_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -23,9 +47,19 @@ namespace SCHOOL.DESKTOP
 
         private void ClassTab_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var classList = _classService.Get();
-            ClassDatagrid.ItemsSource = classList;
+            //var classList = _classService.Get();
+            //ClassDatagrid.ItemsSource = classList;
         }
+
+        private void FetchClasses()
+        {
+            ClassList = _classService.Get(1, 10).Classes;
+        }
+    }
+    public class Item
+    {
+        public string Name { get; set; }
+        public PackIconKind IconKind { get; set; }
     }
 
 }
