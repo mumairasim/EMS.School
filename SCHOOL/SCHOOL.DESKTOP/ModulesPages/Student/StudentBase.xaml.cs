f74dcb8173904129210a497ff54cb8c7251a780d
M Umair Asim <mumairasim@hotmail.com>
2020-10-03 04:26:53 +0500

    Pagination added and minor changes

--- SCHOOL/SCHOOL.DESKTOP/ModulesPages/Student/StudentBase.xaml.cs
+++ SCHOOL/SCHOOL.DESKTOP/ModulesPages/Student/StudentBase.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 using AutoMapper;
@@ -14,6 +15,9 @@ namespace SCHOOL.DESKTOP.ModulesPages.Student
     {
         private readonly IStudentService _studentService;
         private readonly IMapper _mapper;
+        private const int PageSize = 50;
+        public int Page { get; set; }
+
         public StudentBase(IStudentService studentService, IMapper mapper)
         {
             _studentService = studentService;
@@ -23,14 +27,79 @@ namespace SCHOOL.DESKTOP.ModulesPages.Student
         }
         public void PreLoads()
         {
-            var studentList = _studentService.Get(1, 20);
+            Page = 1;
+            var studentList = _studentService.Get(1, PageSize);
             var students = new List<StudentBaseViewModel>();
             _mapper.Map(studentList.Students, students);
             StudentDataGrid.ItemsSource = students;
         }
         public void SearchStudents(object sender, RoutedEventArgs e)
         {
-            var studentList = _studentService.Get(searchStudentTextBox.Text, 1, 20);
+            Page = 1;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, 1, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+        public void rowEditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var row = (StudentBaseViewModel)StudentDataGrid.SelectedItems[0];
+            var updateStudent = new UpdateStudent(row);
+            updateStudent.ShowDialog();
+        }
+
+        private void FirstPage_Click(object sender, RoutedEventArgs e)
+        {
+            Page = 1;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, 1, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+
+        private void PreviousPage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Page > 1)
+            {
+                Page--;
+            }
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, Page, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+
+        private void Page2_Click(object sender, RoutedEventArgs e)
+        {
+            Page = 2;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, 2, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+
+        private void Page3_Click(object sender, RoutedEventArgs e)
+        {
+            Page = 3;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, 3, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            Page++;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, Page, PageSize);
+            var students = new List<StudentBaseViewModel>();
+            _mapper.Map(studentList.Students, students);
+            StudentDataGrid.ItemsSource = students;
+        }
+
+        private void LastPage_Click(object sender, RoutedEventArgs e)
+        {
+            Page = 1;
+            var studentList = _studentService.Get(SearchStudentTextBox.Text, 1, PageSize);
             var students = new List<StudentBaseViewModel>();
             _mapper.Map(studentList.Students, students);
             StudentDataGrid.ItemsSource = students;
---
+++ SCHOOL/SCHOOL.DESKTOP/ModulesPages/Student/UpdateStudent.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using SCHOOL.DTOs.ViewModels.Student;
+
+namespace SCHOOL.DESKTOP.ModulesPages.Student
+{
+    /// <summary>
+    /// Interaction logic for UpdateStudent.xaml
+    /// </summary>
+    public partial class UpdateStudent : Window
+    {
+        public UpdateStudent(StudentBaseViewModel model)
+        {
+            InitializeComponent();
+            MapData(model);
+        }
+
+        public void MapData(StudentBaseViewModel model)
+        {
+            PersonName.Text = model.PersonName;
+            ClassName.Text = model.ClassName;
+            RegistrationNumber.Text = model.RegistrationNumber.ToString();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
