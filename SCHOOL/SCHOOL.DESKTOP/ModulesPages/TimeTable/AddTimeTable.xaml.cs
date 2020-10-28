a66b3aac2dda49d47d15709ae7bf2a1f05f9ba65
M Umair Asim <mumairasim@hotmail.com>
2020-10-28 19:24:11 +0500

    minor changes

--- SCHOOL/SCHOOL.DESKTOP/ModulesPages/TimeTable/AddTimeTable.xaml.cs
+++ SCHOOL/SCHOOL.DESKTOP/ModulesPages/TimeTable/AddTimeTable.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using SCHOOL.DTOs.Enums;
 
 namespace SCHOOL.DESKTOP.ModulesPages.TimeTable
@@ -25,109 +14,83 @@ namespace SCHOOL.DESKTOP.ModulesPages.TimeTable
         public AddTimeTable()
         {
             InitializeComponent();
-            DataGridTimeTableInitialization();
         }
 
-        public DataTable Dt { get; set; }
-        public DataRow Dr { get; set; }
-
-        private void DataGridTimeTableInitialization()
-        {
-            Dt = new DataTable("timeTable");
-
-            DataColumn dc1 = new DataColumn("Days", typeof(string));
-
-            Dt.Columns.Add(dc1);
-
-            TimeTableDataGrid.ItemsSource = Dt.DefaultView;
-        }
         private void Monday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Monday.ToString());
+            AddColumn(DaysOfWeek.Monday.ToString());
         }
 
         private void Tuesday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Tuesday.ToString());
+            AddColumn(DaysOfWeek.Tuesday.ToString());
         }
 
         private void Wednesday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Wednesday.ToString());
+            AddColumn(DaysOfWeek.Wednesday.ToString());
         }
 
         private void Thursday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Thursday.ToString());
+            AddColumn(DaysOfWeek.Thursday.ToString());
         }
 
         private void Friday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Friday.ToString());
+            AddColumn(DaysOfWeek.Friday.ToString());
         }
 
         private void Saturday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Saturday.ToString());
+            AddColumn(DaysOfWeek.Saturday.ToString());
         }
 
         private void Sunday_Checked(object sender, RoutedEventArgs e)
         {
-            AddDayRow(DaysOfWeek.Sunday.ToString());
+            AddColumn(DaysOfWeek.Sunday.ToString());
         }
 
         private void Monday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Monday.ToString());
+            RemoveColumn(DaysOfWeek.Monday.ToString());
         }
         private void Tuesday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Tuesday.ToString());
+            RemoveColumn(DaysOfWeek.Tuesday.ToString());
         }
         private void Wednesday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Wednesday.ToString());
+            RemoveColumn(DaysOfWeek.Wednesday.ToString());
         }
         private void Thursday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Thursday.ToString());
+            RemoveColumn(DaysOfWeek.Thursday.ToString());
         }
         private void Friday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Friday.ToString());
+            RemoveColumn(DaysOfWeek.Friday.ToString());
         }
         private void Saturday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Saturday.ToString());
+            RemoveColumn(DaysOfWeek.Saturday.ToString());
         }
         private void Sunday_Unchecked(object sender, RoutedEventArgs e)
         {
-            RemoveDayRow(DaysOfWeek.Sunday.ToString());
+            RemoveColumn(DaysOfWeek.Sunday.ToString());
         }
 
-
-
-
-        private void RemoveDayRow(string day)
+        private void AddColumn(string columnName)
         {
-            for (int i = 0; i < Dt.Rows.Count; i++)
-            {
-                DataRow dr = Dt.Rows[i];
-                if ((string)dr[0] == day)
-                {
-                    dr.Delete();
-                    Dt.AcceptChanges();
-                    break;
-                }
-            }
-            TimeTableDataGrid.ItemsSource = Dt.DefaultView;
+            DataGridTextColumn textColumn = new DataGridTextColumn { Header = columnName };
+            TimeTableDataGrid.Columns.Add(textColumn);
         }
-        private void AddDayRow(string day)
+
+        private void RemoveColumn(string columnName)
         {
-            Dr = Dt.NewRow();
-            Dr[0] = day;
-            Dt.Rows.Add(Dr);
-            TimeTableDataGrid.ItemsSource = Dt.DefaultView;
+            var textColumn = TimeTableDataGrid.Columns.FirstOrDefault(c => c.Header.Equals(columnName));
+            TimeTableDataGrid.Columns.Remove(textColumn);
         }
+
     }
 }
