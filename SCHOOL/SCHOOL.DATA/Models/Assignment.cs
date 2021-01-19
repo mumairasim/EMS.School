6b96bc1494d6f0c975185b8cba48c3c7da56e689
zeeshan ashraf <zee.digitalivy@outlook.com>
2021-01-19 22:01:28 +0500

    zeeshan ; test change

--- SCHOOL/SCHOOL.DATA/Models/Assignment.cs
+++ SCHOOL/SCHOOL.DATA/Models/Assignment.cs
@@ -21,6 +21,7 @@ namespace SCHOOL.DATA.Models
         public Guid? InstructorId { get; set; }
 
 
+
         public virtual Employee Employee { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
